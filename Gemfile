ee366caab42a7abb0b8e69b7474d0d25426e7482
Razi Syed <candy.razi@gmail.com>
2021-06-29 23:01:39 +0100

    initial

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem 'spree', '~> 4.2'
+gem 'spree_auth_devise', '~> 4.3'
+gem 'rubyzip', '~> 1.2.1'
+gem 'state_machine', '~> 1.2.0'
+gem "paperclip", "~> 6.0.0"
+gem 'sprockets-helpers', '~> 1.2.1'
+
+gemspec
---
+++ lib/spree_themes/version.rb
@@ -0,0 +1,18 @@
+module SpreeThemes
+  module_function
+
+  # Returns the version of the currently loaded SpreeThemes as a
+  # <tt>Gem::Version</tt>.
+  def version
+    Gem::Version.new VERSION::STRING
+  end
+
+  module VERSION
+    MAJOR = 0
+    MINOR = 0
+    TINY  = 1
+    
+
+    STRING = [MAJOR, MINOR, TINY].compact.join('.')
+  end
+end
